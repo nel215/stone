f8bf037126b9e3897c6d55d16ec46dd71d8eeefb
nel215 <otomo.yuhei@gmail.com>
2016-10-02 21:16:38 +0900

    Add exception constructor

--- src/main/java/stone/ParseException.java
+++ src/main/java/stone/ParseException.java
@@ -5,6 +5,21 @@ import java.io.IOException;
 public class ParseException extends Exception {
     static final long serialVersionUID = 0L;
 
+    public ParseException(Token t) {
+        this("", t);
+    }
+
+    public ParseException(String msg, Token t) {
+        super("syntax error around " + location(t) + ". " + msg);
+    }
+
+    private static String location(Token t) {
+        if (t == Token.EOF) {
+            return "the last line";
+        }
+        return "\"" + t.getText() + "\" at line " + t.getLineNumber();
+    }
+
     public ParseException(String msg) {
         super(msg);
     }
