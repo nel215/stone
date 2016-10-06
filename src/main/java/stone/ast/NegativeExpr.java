71085db9900f23d6e1ed731af5df1f604b99d3af
nel215 <otomo.yuhei@gmail.com>
2016-10-07 00:40:09 +0900

    Add NegativeExpr

---
+++ src/main/java/stone/ast/NegativeExpr.java
@@ -0,0 +1,19 @@
+package stone.ast;
+
+import java.util.List;
+
+public class NegativeExpr extends ASTList {
+
+    public NegativeExpr(List<ASTree> list) {
+        super(list);
+    }
+
+    public ASTree operand() {
+        return child(0);
+    }
+
+    public String toString() {
+        return "-" + operand();
+    }
+
+}
