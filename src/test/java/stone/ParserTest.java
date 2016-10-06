b5d10ecf0c48678d077c9fdd491c69a1970a2463
nel215 <otomo.yuhei@gmail.com>
2016-10-07 00:40:50 +0900

    Add factor test (incomplete)

--- src/test/java/stone/ParserTest.java
+++ src/test/java/stone/ParserTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import stone.ast.ASTree;
+import stone.ast.NegativeExpr;
 
 public class ParserTest {
     @Test
@@ -16,5 +17,14 @@ public class ParserTest {
         ASTree ast = adder.parse(l);
         Assert.assertNotNull(ast);
         Assert.assertEquals("(1 + 2)", ast.toString());
+    }
+
+    @Test
+    public void testFactor() throws ParseException {
+        Parser factor = Parser.rule().or(Parser.rule(NegativeExpr.class).sep("-"), Parser.rule());
+        Lexer l = new Lexer(new StringReader("-1"));
+        ASTree ast = factor.parse(l);
+        Assert.assertNotNull(ast);
+        Assert.assertEquals("()", ast.toString());
     }
 }
